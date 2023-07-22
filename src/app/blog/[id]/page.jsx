09a9fc31a1481cf7cfc1ddc519d2b0ad35438857
arinjoy21PEC <114183787+arinjoy21PEC@users.noreply.github.com>
2023-07-22 22:46:27 +0530

    Update page.jsx

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 
 async function getData(id) {
-  const res = await fetch(`https://dailyblog-ui.onrender.com/api/post/${id}`, {
+  const res = await fetch(`/api/post/${id}`, {
     cache: "no-store",
   });
 
