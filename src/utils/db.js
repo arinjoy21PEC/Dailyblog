a2b5c73af6e3501e89649b33f8547589e6bc9606
arinjoy21PEC <114183787+arinjoy21PEC@users.noreply.github.com>
2023-07-23 01:46:49 +0530

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connect = async () => {
   try {
-    await mongoose.connect('mongodb+srv://arinjoynandy:arinjoynandy@cluster0.z4j6ohm.mongodb.net/test?retryWrites=true&w=majority');
+    await mongoose.connect(process.env.MONGO);
   } catch (error) {
     throw new Error("Connection failed!");
   }
