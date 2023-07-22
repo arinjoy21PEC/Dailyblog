4405f47e585099d556087938ae451735fc6378fd
arinjoy21PEC <114183787+arinjoy21PEC@users.noreply.github.com>
2023-07-22 21:50:49 +0530

    Update page.jsx

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 async function getData() {
-  const res = await fetch("http://localhost:3000/api/post", {
+  const res = await fetch("dailyblog-bs73-blytco5r2-arinjoy21pec.vercel.app/api/post", {
     cache: "no-store",
   });
 
