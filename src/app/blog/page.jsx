ee1825631fa605ac2f6444f63f6c706024c7d58a
arinjoy21PEC <114183787+arinjoy21PEC@users.noreply.github.com>
2023-07-22 22:27:54 +0530

    Update page.jsx

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 async function getData() {
-  const res = await fetch("https://dailyblog-si1o.vercel.app/api/post", {
+  const res = await fetch("https://dailyblog-ui.onrender.com/api/post", {
     cache: "no-store",
   });
 
