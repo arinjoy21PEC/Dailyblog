697dd937ee104a015dafe6c57a32acdfd5d20f33
arinjoy21PEC <114183787+arinjoy21PEC@users.noreply.github.com>
2023-07-22 22:48:38 +0530

    Update page.jsx

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,16 +4,16 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 
 async function getData(id) {
-  const res = await fetch(`/api/post/${id}`, {
-    cache: "no-store",
-  });
+//   const res = await fetch(`/api/post/${id}`, {
+//     cache: "no-store",
+//   });
 
-  if (!res.ok) {
-    return notFound()
-  }
+//   if (!res.ok) {
+//     return notFound()
+//   }
 
-  return res.json();
-}
+//   return res.json();
+// }
 
 
 export async function generateMetadata({ params }) {
