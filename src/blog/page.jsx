da8aaf39eb2370307d5bf8c96fd6a7390113cdb2
arinjoy21PEC <114183787+arinjoy21PEC@users.noreply.github.com>
2023-07-23 01:23:41 +0530

    Update page.jsx

--- src/blog/page.jsx
+++ src/blog/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
-import Image from "next/image";
+import Image from "next/image"
 
 async function getData() {
   const res = await fetch(`${process.env.NEXTAUT_URL}/api/post`, {
