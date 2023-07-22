abec73f5a7ef4e3a6ba3f3a415ab0ab1cc07450a
arinjoy21PEC <114183787+arinjoy21PEC@users.noreply.github.com>
2023-07-22 21:51:59 +0530

    Update page.jsx

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 
 async function getData(id) {
-  const res = await fetch(`dailyblog-bs73-blytco5r2-arinjoy21pec.vercel.app/api/post/${id}`, {
+  const res = await fetch(`https://dailyblog-bs73.vercel.app/api/post/${id}`, {
     cache: "no-store",
   });
 
